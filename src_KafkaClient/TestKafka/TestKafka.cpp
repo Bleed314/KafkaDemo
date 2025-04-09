b43ec853aeaefae4bbf5b69b0b46b075c398bda2
Bleed314 <2362880148@qq.com>
2025-04-09 16:13:55 +0800

    v1

--- src_KafkaClient/TestKafka/TestKafka.cpp
+++ src_KafkaClient/TestKafka/TestKafka.cpp
@@ -14,34 +14,52 @@ const string BROKER_LIST = "192.168.131.128:9092";  // Kafka broker 地址
 const string TOPIC_NAME = "test1"; // 测试主题
 
 int main() {
-    cppkafka::Configuration config = {
-        {"metadata.broker.list", BROKER_LIST},
-        {"group.id", "my_consumer_group"},
-        {"auto.offset.reset", "latest"}  // 设置为"latest"来接收实时消息
+	// 消费者配置示例
+    //cppkafka::Configuration config = {
+    //    {"metadata.broker.list", BROKER_LIST},
+    //    {"group.id", "my_consumer_group"},
+    //    {"auto.offset.reset", "latest"}  // 设置为"latest"来接收实时消息
+    //};
+
+    //cppkafka::Consumer consumer(config);
+    //consumer.subscribe({ TOPIC_NAME });
+
+    //while (true) {
+    //    cppkafka::Message msg = consumer.poll();
+    //    if (msg) {
+    //        // 处理消息
+    //        std::cout << "Received message: " << msg.get_payload() << std::endl;
+    //    }
+    //}
+
+	// 生产者配置示例
+        // Construct the configuration
+    Configuration config = {
+        { "metadata.broker.list", BROKER_LIST }
     };
 
-    cppkafka::Consumer consumer(config);
-    consumer.subscribe({ TOPIC_NAME });
-
-    while (true) {
-        cppkafka::Message msg = consumer.poll();
-        if (msg) {
-            // 处理消息
-            std::cout << "Received message: " << msg.get_payload() << std::endl;
+    // Create the producer
+    MessageBuilder builder(TOPIC_NAME);
+    Producer producer(config);
+    builder.partition(-1);
+    // Now read lines and write them into kafka
+    string line;
+    while (getline(cin, line)) {
+        if (cin.fail()) {
+            cerr << "从输入流读取时出错" << endl;
+            break;
         }
+        // Set the payload on this builder
+        builder.payload(line);
+
+        // Actually produce the message we've built
+        producer.produce(builder);
     }
+    producer.flush();
+
+    // Flush all produced messages
+  
 
     return 0;
 }
 
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
